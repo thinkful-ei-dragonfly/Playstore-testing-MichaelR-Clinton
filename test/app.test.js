d0662abda2b37d7a3b7448b2c3e98321fa483a65
Airstryk-1 <cj2027@yahoo.com>
2019-06-12 13:29:34 -0700

    finished all test cases

--- test/app.test.js
+++ test/app.test.js
@@ -2,8 +2,8 @@
 
 const expect = require('chai').expect;
 const supertest = require('supertest');
-
 const app = require('../app');
+
 
 describe('app', () => {
   it('should return a list of apps',() => {
@@ -18,4 +18,91 @@ describe('app', () => {
       });
   });
 
+  it('should be 400 if sort is incorrect', () => {
+    return supertest(app)
+    .get('/apps')
+    .query({sort: 'MISTAKE'})
+      .expect(400, 'invalid sort category, please enter rating or app');
+  });
+
+  it('should sort by rating', () => {
+    return supertest(app)
+      .get('/apps')
+      .expect(200)
+      .query({sort: 'rating'})
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let i = 0;
+        let sorted = true;
+        while (sorted && i < res.body.length - 1) {
+          sorted = sorted && res.body[i].Rating <= res.body[i + 1].Rating;
+          i++
+        }
+        expect(sorted).to.be.true;
+      })
+  });
+
+  it('should sort by title', () => {
+    return supertest(app)
+      .get('/apps')
+      .expect(200)
+      .query({sort: 'app'})
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let i = 0;
+        let sorted = true;
+        while (sorted && i < res.body.length - 1){
+          sorted = sorted && res.body[i].App <= res.body[i + 1].App;
+          i++
+        }
+        expect(sorted).to.be.true;
+      })
+  })
+
+  it('should filter by genre', () => {
+    return supertest(app)
+      .get('/apps')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .query({genres: 'Arcade'})
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let filtered = true;
+        for(let i = 0; i < res.body.length; i++){
+          if (!res.body[i].Genres.includes('Arcade')){
+            console.log(res.body[i].Genres);
+            filtered = false;
+          }
+        }
+        expect(filtered).to.be.true;
+      })
+  });
+
+  it('should sort by rating/app and genre', () => {
+    return supertest(app)
+      .get('/apps')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .query({genres: 'Arcade', sort: 'rating'})
+      .then(res => {
+        let i = 0;
+        let sorted = true;
+        while (sorted && i < res.body.length - 1) {
+          sorted = sorted && res.body[i].App <= res.body[i + 1].App;
+          i++
+        }
+        expect(sorted).to.be.true;
+        
+        let filtered = true;
+        for (let i = 0; i < res.body.length; i++) {
+          if (!res.body[i].Genres.includes('Arcade')) {
+            console.log(res.body[i].Genres);
+            filtered = false;
+          }
+        }
+        expect(filtered).to.be.true;
+      })
+  })
 });
